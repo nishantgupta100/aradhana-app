544221645fdf9a10fd398696f2bc3dd49670c2a2
Nishant Gupta <nishantgupta1291@gmail.com>
2026-02-16 20:43:58 +0530

    Update page.js

--- app/book/page.js
+++ app/book/page.js
@@ -1,12 +1,11 @@
-export const dynamic = "force-dynamic";
-
 "use client"
 
+import { Suspense, useState } from "react"
 import { useSearchParams } from "next/navigation"
-import { useState } from "react"
 import { supabase } from "../../lib/supabase"
 
-export default function BookPage() {
+
+function BookingForm() {
 
 const params = useSearchParams()
 
@@ -20,20 +19,17 @@ const [date, setDate] = useState("")
 const [time, setTime] = useState("")
 
 const timeSlots = [
-
 "06:00 AM",
 "07:00 AM",
 "08:00 AM",
 "09:00 AM",
 "10:00 AM",
 "11:00 AM",
-"12:00 PM",
 "04:00 PM",
 "05:00 PM",
-"06:00 PM",
-"07:00 PM"
-
+"06:00 PM"
 ]
+
 
 async function handleBooking() {
 
@@ -52,16 +48,16 @@ alert("Booking Confirmed 🙏")
 
 }
 
+
 return (
 
-<div className="min-h-screen bg-gradient-to-b from-orange-50 to-white flex justify-center items-center">
+<div className="min-h-screen bg-orange-50 flex justify-center items-center">
 
 <div className="bg-white shadow-lg rounded-xl p-8 w-[450px]">
 
-<h1 className="text-2xl font-bold text-orange-600 mb-6 text-center">
+<img src="/om.png" width="60" className="mx-auto mb-4"/>
 
-<img src="/om.png" width="60" className="mx-auto mb-4" />
-
+<h1 className="text-2xl font-bold text-orange-600 text-center mb-6">
 
 {service_name}
 
@@ -69,59 +65,36 @@ return (
 
 
 <input
-
 placeholder="Your Name"
-
 className="border p-3 rounded w-full mb-4"
-
 onChange={(e)=>setName(e.target.value)}
-
 />
 
 
 <input
-
-placeholder="Phone Number"
-
+placeholder="Phone"
 className="border p-3 rounded w-full mb-4"
-
 onChange={(e)=>setPhone(e.target.value)}
-
 />
 
 
 <input
-
 placeholder="Address"
-
 className="border p-3 rounded w-full mb-4"
-
 onChange={(e)=>setAddress(e.target.value)}
-
 />
 
-
-<label className="text-sm">Select Date</label>
 
 <input
-
 type="date"
-
 className="border p-3 rounded w-full mb-4"
-
 onChange={(e)=>setDate(e.target.value)}
-
 />
 
-
-<label className="text-sm">Select Time Slot</label>
 
 <select
-
 className="border p-3 rounded w-full mb-6"
-
 onChange={(e)=>setTime(e.target.value)}
-
 >
 
 <option>Select Time</option>
@@ -136,11 +109,8 @@ onChange={(e)=>setTime(e.target.value)}
 
 
 <button
-
 onClick={handleBooking}
-
-className="bg-orange-500 text-white w-full py-3 rounded-lg hover:bg-orange-600 text-lg"
-
+className="bg-orange-500 text-white w-full py-3 rounded-lg"
 >
 
 Confirm Booking
@@ -150,6 +120,22 @@ Confirm Booking
 </div>
 
 </div>
+
+)
+
+}
+
+
+
+export default function Page(){
+
+return (
+
+<Suspense>
+
+<BookingForm/>
+
+</Suspense>
 
 )
 
